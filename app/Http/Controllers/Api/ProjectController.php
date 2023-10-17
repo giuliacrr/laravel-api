bab0b13d4d92ef20ec7895780d29a63ea69c1594
giuliacrr <giulia.corrado02@gmail.com>
2023-10-17 17:35:55 +0200

    Show in controller e api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,8 +9,21 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){ //stessi nomi con le crud
+        
+
         $projects = Project::with(["type", "technologies"])->paginate(5); //Usiamo sempre lo STESSO MODEL! :D
         
         return response()->json($projects); //ritorno l'array che mi ritrovo con Project::with qui sopra sotto forma di json.
+    }
+
+    public function show($slug) {
+        //cerchiamo lo slug
+        $project = Project::where("slug", $slug)
+            // prendiamo anche le relazioni
+            ->with(["type", "technologies"])
+            // ritorniamo il primo risultato
+            ->first();
+
+        return response()->json($project);
     }
 }
